08102c6fd434fcfa3792da63dee9f036a6f8f3a8
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-01 16:46:02 -0500

    updated IStoreService namespace

--- BaristaBuddyMVC/BaristaBuddyMVC/Services/IStoreService.cs
+++ BaristaBuddyMVC/BaristaBuddyMVC/Services/IStoreService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace BaristaBuddyMVC.Controllers
+namespace BaristaBuddyMVC.Services
 {
     internal interface IStoreService
     {
