4a4b7f95b1245bfaf694c568275f7b962190c33b
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-01 16:54:00 -0500

    added using to StoresController

--- BaristaBuddyMVC/BaristaBuddyMVC/Controllers/StoresController.cs
+++ BaristaBuddyMVC/BaristaBuddyMVC/Controllers/StoresController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BaristaBuddyMVC.Models;
+using BaristaBuddyMVC.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
