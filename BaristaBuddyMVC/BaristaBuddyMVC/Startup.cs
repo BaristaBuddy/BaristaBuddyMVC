82306ebba40695b03dba3d7ab728e3e390674db9
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-01 16:26:57 -0500

    set up connection with localhost

--- BaristaBuddyMVC/BaristaBuddyMVC/Startup.cs
+++ BaristaBuddyMVC/BaristaBuddyMVC/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -24,6 +25,12 @@ namespace BaristaBuddyMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(Configuration.GetValue<string>("API_URL")),
+            };
+            services.AddSingleton<HttpClient>(httpClient);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
