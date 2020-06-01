695a2d4ea775c6f58fc4cd16a0430e736d8280fd
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-01 16:45:08 -0500

    built store interface and controller components

---
+++ BaristaBuddyMVC/BaristaBuddyMVC/Controllers/StoresController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaristaBuddyMVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaristaBuddyMVC.Controllers
+{
+    public class StoresController : Controller
+    {
+        private readonly IStoreService storeService;
+
+        // GET: Stores
+        public async Task<ActionResult<List<Store>>> Index()
+        {
+            var stores = await storeService.GetAllStores();
+            return View(stores);
+        }
+
+        // GET: Stores/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Stores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stores/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Stores/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StoresController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StoresController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StoresController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BaristaBuddyMVC/BaristaBuddyMVC/Services/IStoreService.cs
@@ -0,0 +1,11 @@
+﻿using BaristaBuddyMVC.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BaristaBuddyMVC.Controllers
+{
+    internal interface IStoreService
+    {
+        Task<List<Store>> GetAllStores();
+    }
+}
