385aa4d991e6fecd6f005090a266988d50a1c671
Zscoob <james.zobian@gmail.com>
2020-06-02 16:51:57 -0500

    updates added and saved

--- BaristaBuddyMVC/BaristaBuddyMVC/Services/IItemService.cs
+++ BaristaBuddyMVC/BaristaBuddyMVC/Services/IItemService.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using BaristaBuddyMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BaristaBuddyMVC.Services
 {
-    interface IItemService
+    public interface IItemService
     {
+        Task<List<Item>> GetAllItems();
+        Task<Item> GetOneItem(int id);
+        Task<Item> AddItem(Item item);
+        Task<Item> UpdateItems(int id, Item item);
+        Task DeleteItem(int id);
+
     }
 }
