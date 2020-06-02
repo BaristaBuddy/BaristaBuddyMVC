8b969c921fa4337f7abe70e165bc28d6a2756a34
Zscoob <james.zobian@gmail.com>
2020-06-02 16:46:35 -0500

    HttpItemsService

---
+++ BaristaBuddyMVC/BaristaBuddyMVC/Services/HttpItemsService.cs
@@ -0,0 +1,15 @@
+﻿using BaristaBuddyMVC.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BaristaBuddyMVC.Services
+{
+    public interface HttpItemsService
+    {
+        Task<Item> AddItem(Item item);
+        Task DeleteItem(int id);
+        Task<List<Item>> GetAllItems();
+        Task<Item> GetOneItem(int id);
+        Task<Item> UpdateItems(int id, Item item);
+    }
+}
