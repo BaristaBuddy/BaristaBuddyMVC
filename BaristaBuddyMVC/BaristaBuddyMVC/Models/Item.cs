17555a7858ad876f83d1ab8de5d0d6224e4cc6cf
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-03 12:08:36 -0500

    added JsonNameProperties to model properties!

--- BaristaBuddyMVC/BaristaBuddyMVC/Models/Item.cs
+++ BaristaBuddyMVC/BaristaBuddyMVC/Models/Item.cs
@@ -1,31 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BaristaBuddyMVC.Models
 {
     public class Item
     {
+        [JsonPropertyName("id")]
         public int Id
         {
             get; set;
         }
-
+        [JsonPropertyName("name")]
         public string Name 
         {
             get; set;
         }
-
+        [JsonPropertyName("ingredients")]
         public string Ingredients
         {
             get; set;
         }
-
+        [JsonPropertyName("itemImageUrl")]
         public string ItemImageUrl
         {
             get; set;
         }
+        [JsonPropertyName("price")]
         public decimal Price
         {
             get; set;
