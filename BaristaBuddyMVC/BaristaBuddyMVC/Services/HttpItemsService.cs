df35a42d633260279d96fe5451ecac6cdb134ff2
Zscoob <james.zobian@gmail.com>
2020-06-03 10:10:07 -0500

    routes updated on httpsitemservice

--- BaristaBuddyMVC/BaristaBuddyMVC/Services/HttpItemsService.cs
+++ BaristaBuddyMVC/BaristaBuddyMVC/Services/HttpItemsService.cs
@@ -39,31 +39,31 @@ namespace BaristaBuddyMVC.Services
 
      
 
-        public async Task DeleteItem(int id)
+        public async Task DeleteItem(int id, int storeId)
         {
-            var response = await client.DeleteAsync($"Stores/Items/{id}");
+            var response = await client.DeleteAsync($"Stores/{storeId}/Items/{id}");
             response.EnsureSuccessStatusCode();
         }
 
-             public async Task<List<Item>> GetAllItems()
+             public async Task<List<Item>> GetAllItems(int storeId)
         {
-            var responseStream = await client.GetStreamAsync("Stores/Items");
+            var responseStream = await client.GetStreamAsync($"Stores/{storeId}/Items");
             List<Item> result = await JsonSerializer.DeserializeAsync<List<Item>>(responseStream);
             return result;
         }
 
-            public async Task<Item> GetOneItem(int id)
+            public async Task<Item> GetOneItem(int id, int storeId)
         {
-            var responseStream = await client.GetStreamAsync($"stores/Items/{id}");
+            var responseStream = await client.GetStreamAsync($"stores/{storeId}/Items/{id}");
             Item result = await JsonSerializer.DeserializeAsync<Item>(responseStream);
             return result;
         }
 
-             public async Task<Item> UpdateItems(int id, Item item)
+             public async Task<Item> UpdateItems(int id, Item item, int storeId)
         {
             using (var content = new StringContent(JsonSerializer.Serialize(item), System.Text.Encoding.UTF8, "Application/Json"))
             {
-                var response = await client.PutAsync($"items/{id}", content);
+                var response = await client.PutAsync($"Stores/{storeId}/items/{id}", content);
                 if(response.StatusCode  == System.Net.HttpStatusCode.NoContent)
                 {
                     return item;
