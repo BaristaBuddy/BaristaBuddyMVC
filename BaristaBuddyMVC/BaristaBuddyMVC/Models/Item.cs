43b07793dc93b0db324893a585b27a7859e931d4
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-05 01:24:54 -0500

    modified Item model with readability properties

--- BaristaBuddyMVC/BaristaBuddyMVC/Models/Item.cs
+++ BaristaBuddyMVC/BaristaBuddyMVC/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -9,36 +10,27 @@ namespace BaristaBuddyMVC.Models
     public class Item
     {
         [JsonPropertyName("itemId")] 
-        public int Id
-        {
-            get; set;
-        }
+        public int Id { get; set; }
+
         [JsonPropertyName("name")]
-        public string Name 
-        {
-            get; set;
-        }
+        public string Name { get; set; }
+
         [JsonPropertyName("ingredients")]
-        public string Ingredients
-        {
-            get; set;
-        }
+        public string Ingredients { get; set; }
+
         [JsonPropertyName("imageUrl")]
-        public string ItemImageUrl
-        {
-            get; set;
-        }
+        [Display(Name = "Image URL")]
+        public string ItemImageUrl { get; set; }
+
         [JsonPropertyName("price")]
-        public decimal Price
-        {
-            get; set;
-        }
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal Price { get; set; }
+
         [JsonPropertyName("storeId")]
         public int StoreId { get; set; }
 
         [JsonPropertyName("itemModifiers")]
-
-     
+        [Display(Name = "Modifiers")]
         public List<ItemModifier> ItemModifiers { get; set; }
 
     }
